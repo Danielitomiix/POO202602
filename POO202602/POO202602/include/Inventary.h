0141df6a18fd94ef8dd0a3e0a99cf16b586291a8
Daniel Vargas <porta_force@hotmail.com>
2025-09-12 10:39:47 -0600

    Add inventory and item system with Character class
    Introduced Inventary and Item classes to manage an inventory system, including methods to add, use, and display items. Updated main.cpp to demonstrate inventory usage and added a Character class with health and rebirth logic. Also updated Prerequisitos.h to include <ctime>.

---
+++ POO202602/POO202602/include/Inventary.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "Prerequisitos.h"
+#include "Item.h"
+
+class Inventary {
+public:
+	Inventary() = default;
+	~Inventary() = default;
+
+private:
+	std::vector<Item> m_items;
+
+	void
+		addItem(const std::string& name, int   quiantity) {
+		// Buscar si el item ya existe y sumarle la cantidad
+		for (auto& item : m_items) {
+			if (item.getName() == name) {
+				item.add(quiantity);
+				return;
+			}
+		}
+		// Si no existe, agregar un nuevo item
+		m_items.emplace_back(name, quiantity);
+	}
+
+	void
+		showInventory() const {
+		// verificar si el inventario esta vacio
+		if (m_items.empty()) {
+			std::cout << "El inventario esta vacio" << std::endl;
+			return;
+		}
+
+		// Mostrar los items en el inventario
+		std::cout << "Inventario:" << std::endl;
+		for (const auto& item : m_items) {
+			std::cout << " - " << item.getName() << ": " << item.getQuantity() << std::endl;
+		}
+	}
+
+
+	void
+		useItem(const std::string& name, int quantity) {
+		for (auto& item : m_items) {
+			if (item.getName() == name) {
+				item.remove(quantity);
+				// Si la cantidad del item es 0, eliminarlo del inventario
+				if (item.getQuantity() == 0) {
+					m_items.erase(std::remove(m_items.begin(), m_items.end(), item), m_items.end());
+				}
+				std::cout << "Usaste " << quantity << " de " << name << std::endl;
+				return;
+			}
+		}
+		std::cout << "No tienes " << name << " en el inventario" << std::endl;
+	}
+
+};
--- POO202602/POO202602/source/main.cpp
+++ POO202602/POO202602/source/main.cpp
@@ -1,5 +1,59 @@
 #include "Prerequisitos.h"
+#include "Inventary.h"
+#include "Item.h"
 
-int main() {
+
+class
+	Character {
+public:
+	Character() = default;
+
+		Character(int healt) : m_healt(healt) {}
+
+	void
+		rebirth() {
+		if (!isCharacterAlive && m_healt <= 0) {
+			std::cout << "Character is alive" << std::endl;
+			isCharacterAlive = true;
+		}
+	}
+
+	int
+		getHealt() const {
+		return m_healt;
+	}
+
+	void
+		setHealt(int healt) {
+		m_healt = healt;
+	}
+
+private:
+	int m_healt;
+	bool isCharacterAlive = false;
+
+protected:
+	int m_healt;
+};
+
+
+int
+main() {
+	Inventary inventary;
+	inventary.addItem("Potion", 5);
+	inventary.addItem("Elixir", 3);
+	inventary.addItem("Diamante", 2);
+
+	inventary.useItem("Potion", 2);
+	inventary.showInventory();
+
+
+	Rectangulo rect(10.0, 5.0);
+	std::cout << "Area: " << rect.calcularArea() << std::endl;
+	std::cout << "Perimetro: " << rect.calcularPerimetro() << std::endl;
+
+
+	std::cout << "Hello World!" << std::endl;
+	std::cin.get();
 	return 0;
-}	
+}
