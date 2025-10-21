d95c692ecbae7ef0d3baf597aa99e524e00180d0
Daniel Vargas <porta_force@hotmail.com>
2025-10-21 09:20:20 -0600

    Update main2.cpp

--- POO202602/POO202602/source/main2.cpp
+++ POO202602/POO202602/source/main2.cpp
@@ -1,56 +1,20 @@
 #include "Prerequisitos.h"
-#include "ProgramminPatterns/MiSingleton.h"
-#include "ProgramminPatterns/RegistroActividad.h"
-#include "ProgramminPatterns/FactoryMethod.h"
-#include "ProgramminPatterns/FactoryMethodVehiculos.h"
+#include "ProgramminPatterns/AbstractFactory/FabricaConcreta.h"
 
-
-
-MiSingleton* MiSingleton::instancia = nullptr;
-RegistroActividad* RegistroActividad::instancia = nullptr;
 
 
 int main() {
 
-  RegistroActividad* registro1 = RegistroActividad::getInstance();
-  registro1->AgregarActividad("Inicio de sesion");
-  registro1->AgregarActividad("abrio el menu");
+  FabricaAbstracta* fabrica = new FabricaConcreta();
+  ProductoA* productoA = fabrica->crearProductoA();
+  ProductoB* productoB = fabrica->crearProductoB();
 
-  RegistroActividad* registro2 = RegistroActividad::getInstance();
-  registro2->AgregarActividad("cerro sesion");
+  productoA->operacionA();
+  productoB->operacionB();
 
-  registro2->MostrarActividades();
-
-
-  MiSingleton* singleton = MiSingleton::getInstance();
-  singleton->setData(42);
-
-  std::cout << "Valor del singleton: " << singleton->getData() << std::endl;
-
-  MiSingleton* singleton2 = MiSingleton::getInstance();
-  std::cout << "Valor del singleton: " << singleton->getData() << std::endl;
-
-
-  Fabrica* fabrica = new FabricaA(); //Fabrica Genera telsa(FabricaA)
-  Producto* producto = fabrica->CrearProducto(); //Producto tesla(ProductoA)
-  producto->Operacion();
-
-  delete producto;
   delete fabrica;
-
-  FactoriaVehiculos* fabricaVehiculos = new FactoriaCoches(); 
-  Vehiculo* miVehiculo = fabricaVehiculos->crearVehiculo();
-  miVehiculo->descripcion();
-
-  delete miVehiculo;
-  delete fabricaVehiculos;
-
-  FactoriaVehiculos* fabricaBicis = new FactoriaBicicletas();
-  Vehiculo* miBici = fabricaBicis->crearVehiculo();
-  miBici->descripcion();
-
-  delete miBici;
-  delete fabricaBicis;
+  delete productoA;
+  delete productoB;
 
 
 
